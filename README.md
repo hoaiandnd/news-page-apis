37ac8e7681bc11ec3cb0aff8c3afe59c0f835d1e
ngohoaian <ngohoaian01031@gmail.com>
2025-04-12 16:51:14 +0700

    update

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# Lưu ý
-- Repo này chỉ xây dựng cấu trúc cho API dùng Express.js
-- Nên sử dụng `git clone` để kéo ứng dụng này về để sử dụng bản sao
-- Chỉ định lại file `.gitignore` cho phù hợp với dự án
+# News website APIs
--- src/utils/controller.util.js
+++ src/utils/controller.util.js
@@ -1,47 +1,50 @@
 const { message: responseMessage } = require('../constants/response.const')
 
+const injectResponseMethods = res => ({
+  /**
+   * @param {{ message?: string; data?: any; status?: number }} json
+   */
+  ok: json => {
+    const { message = responseMessage.success.oK, data = null, status = 200 } = json
+    res.status(status).json({ message, data })
+  },
+  /**
+   *
+   * @param {*} res Response object
+   * @param {{ message?: string; data?: any; status?: number }} json
+   */
+  internalServerError: (res, json) => {
+    const { message = responseMessage.fail.internalServerError, data = null, status = 500 } = json
+    res.status(status).json({ message, data })
+  },
+
+  /**
+   *
+   * @param {*} res Response object
+   * @param {{ message?: string; data?: any; status?: number }} json
+   */
+  badRequest: (res, json) => {
+    const { message = responseMessage.fail.badRequest, data = null, status = 400 } = json
+    res.status(status).json({ message, data })
+  }
+})
+
 /**
  * * Converts an action function to a middleware function.
  * * Combine the `request`, `response`, and `next` parameters into a single object.
- * * Inject `res.ok`, `res.internalError`, and `res.badRequest` methods into the response object.
+ * * Inject response methods into the `response` object.
  * @param {(httpContext: {req: any; res: any; next: any}) => Promise<any>} action action function return a promise
  * @returns {function} middleware function
  */
 const fromAction = action => {
   return async function (req, res, next) {
     try {
-      res = { ...res, ok, internalError: internalServerError, badRequest }
+      res = { ...res, ...injectResponseMethods(res) }
       await action({ req, res, next })
     } catch (error) {
       next(error)
     }
   }
-}
-/**
- * @param {{ message?: string; data?: any; status?: number }} json
- */
-const ok = json => {
-  const { message = responseMessage.success.oK, data = null, status = 200 } = json
-  res.status(status).json({ message, data })
-}
-/**
- *
- * @param {*} res Response object
- * @param {{ message?: string; data?: any; status?: number }} json
- */
-const internalServerError = (res, json) => {
-  const { message = responseMessage.fail.internalServerError, data = null, status = 500 } = json
-  res.status(status).json({ message, data })
-}
-
-/**
- *
- * @param {*} res Response object
- * @param {{ message?: string; data?: any; status?: number }} json
- */
-const badRequest = (res, json) => {
-  const { message = responseMessage.fail.badRequest, data = null, status = 400 } = json
-  res.status(status).json({ message, data })
 }
 
 module.exports = { fromAction }
